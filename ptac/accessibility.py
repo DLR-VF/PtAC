7bd67b560b0e1ee3df8a7c0e7651fc696b54a680
Yosmaoglu <Serra.Yosmaoglu@dlr.de>
2021-07-19 16:34:03 +0200

    include population data conversion from raster to points

--- ptac/accessibility.py
+++ ptac/accessibility.py
@@ -6,6 +6,14 @@ import timeit
 import pandas as pd
 import osmnx as ox
 
+import sys
+
+
+sys.path.insert(0, os.path.abspath('..'))
+import numpy as np
+import rasterio
+from affine import Affine
+import geopandas as gpd
 
 def prepare_origins_and_destinations(dest_gdf, od="origin"):
     """
@@ -228,6 +236,14 @@ def distance_to_closest(start_geometries,
                                                          maximum_distance=maximum_distance)
     return accessibility_output
 
+def concat_dfs(input):
+    if isinstance(input, list):
+        # concatenate dataframes
+        df = pd.concat(input)
+        # drop duplicates:
+        df = df.drop_duplicates(subset=['index', 'o_id'])
+    return df
+
 # calculate SDG 11.2.1 indicator: pass input either as one dataframe or a list of two dataframes
 def calculate_sdg(population, input, population_column):
     total_population = population[population_column].sum()
@@ -254,3 +270,67 @@ def calculate_sdg(population, input, population_column):
         sdg = accessibility_output_population / total_population
         print("SDG 11.2.1 indicator is calculated")
     return sdg
+
+
+def raster_to_points(path, band=1, epsg=4326):
+    """
+    :param path: path to raster file. (Tested with GeoTIF)
+    :type path: String
+    :param band: Band of dataset
+    :type band: int
+    :return: Point GeoDataFrame including Raster values of specific band
+    :rtype: GeoPandas.GeoDataFrame:: Point
+    """
+
+    with rasterio.open(path) as ds:
+        arr = ds.read(band)
+        t0 = ds.transform
+
+    t1 = t0 * Affine.translation(0.5, 0.5)
+
+    # Needed to move Affine object to left side to get rid of
+    # DeprecationWarning: Right multiplication will be prohibited in version 3.0
+    rc2xy = lambda r, c: t1 * (c, r)
+
+    x_coordinates = []
+    y_coordinates = []
+    values = []
+
+    for x in range(0, arr.shape[0]):
+        for y in range(0, arr.shape[1]):
+            if not np.isnan(arr[x, y]) and arr[x, y] != 0.0:
+                values.append(arr[x, y])
+                x_coordinates.append(rc2xy(x, y)[0])
+                y_coordinates.append(rc2xy(x, y)[1])
+
+    df = pd.DataFrame({
+        "pop": values,
+        "x_coord": x_coordinates,
+        "y_coord": y_coordinates})
+    geo_df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x_coord, df.y_coord), crs=f"EPSG:{epsg}")
+
+    return geo_df
+
+# creates population dataset in gpkg, csv and shp file formats:
+def create_population_data(input_path, output_path, extension):
+    cities = os.listdir(rf'{input_path}')
+    for city in cities:
+        # print(city)
+        if city.endswith("tif"):
+            print(f"processing {city}")
+            raster_path = rf'{input_path}/{city}'
+            if extension == "gpkg":
+                points_gdf = raster_to_points(raster_path, band=1, epsg=4326)
+                city_name = city.split(".")[0].lower()
+                points_gdf.to_file(f"{output_path}/{city_name}_pop_point.{extension}", driver="GPKG")
+            elif extension == "shp":
+                points_gdf = raster_to_points(raster_path, band=1, epsg=4326)
+                city_name = city.split(".")[0].lower()
+                points_gdf.to_file(f"{output_path}/{city_name}_pop_point.{extension}", driver="ESRI Shapefile")
+            elif extension == "csv":
+                points_gdf = raster_to_points(raster_path, band=1, epsg=4326)
+                city_name = city.split(".")[0].lower()
+                points_gdf.to_csv(f"{output_path}/{city_name}_pop_point.{extension}", sep=";", header=False)
+            else:
+                print(f"{extension} extension is not supported")
+
