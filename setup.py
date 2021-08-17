b47945ee0dec1c964737eb0ad5090892e4bf379e
Yosmaoglu <Serra.Yosmaoglu@dlr.de>
2021-08-17 18:52:13 +0200

    add install_requires and license

--- setup.py
+++ setup.py
@@ -10,21 +10,21 @@ def readme():
     with open('README.md') as f:
         return f.read()
 setup(
-        name='ptac',
-        version='0.0.4',
+        name="ptac",
+        version="0.0.4",
         author="Simon Nieland, Serra Yosmaoglu",
         author_email="Simon.Nieland@dlr.de, Serra.Yosmaoglu@dlr.de",
         long_description=readme(),
         long_description_content_type='text/markdown',
-        url='https://github.com/DLR-VF/PtAC',
+        url="https://github.com/DLR-VF/PtAC",
         classifiers=[
             'Development Status :: 5 - Production/Stable', 
             'Programming Language :: Python :: 3', 
             'Operating System :: OS Independent',
             'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)'
             ],
-        licence='',
+        licence="Eclipse Public License 2.0 (EPL-2.0)",
         include_package_data=True,
-        packages=find_packages(exclude=['test']), #['ptac'],
-        install_requires=[],
+        packages=find_packages(exclude=["test"]),
+        install_requires=["osmnx", "rasterio"],
         zip_safe=False)
