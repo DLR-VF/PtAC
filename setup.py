fccc44c93140d16b5eec4c986902d590d0c4ba50
vf-user <vf-user@vf-artemis.intra.dlr.de>
2022-06-01 14:44:27 +0200

    configure setup

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ See license in LICENSE
 from setuptools import setup
 
 
-def readme():
-    with open("README.md") as f:
-        return f.read()
+# def readme():
+    # with open("README.md") as f:
+        # return f.read()
 
 
 if __name__ == "__main__":
