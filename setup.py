064341e6c5c34506d8cbc9b3a4e17e0c972ae9dd
Yosmaoglu <Serra.Yosmaoglu@dlr.de>
2022-08-26 16:00:55 +0200

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name="ptac",
-    version="0.0.1b1",
+    version="0.1.3",
     author='Simon Nieland, Serra Yosmaoglu',
     author_email='Simon.Nieland@dlr.de, Serra.Yosmaoglu@dlr.de',
     long_description=readme(),
