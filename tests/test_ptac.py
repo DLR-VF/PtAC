5808ea056cdc532551f18468dac391ff8592a220
Simon nieland <simon.nieland@mailbox.org>
2022-10-17 16:08:29 +0200

    conda instead of mamba

--- tests/test_ptac.py
+++ tests/test_ptac.py
@@ -2,13 +2,15 @@
 Unit tests for PtAC library
 """
 
-import geopandas as gpd
 import pathlib
+import sys
+import unittest
+
+import geopandas as gpd
 import ptac.accessibility as accessibility
 import ptac.population as population
 import ptac.util as util
-import sys
-import unittest
+
 
 class PtACTest(unittest.TestCase):
     def set_up(self):
@@ -23,17 +25,32 @@ class PtACTest(unittest.TestCase):
 
     def test_prepare_network(self):
         self.set_up()
-        df_prepare_network = accessibility.prepare_network(network_gdf=self.net, boundary=self.boundary)
+        df_prepare_network = accessibility.prepare_network(
+            network_gdf=self.net, boundary=self.boundary
+        )
         value = df_prepare_network["index"].count()
         self.assertEqual(value, 50)
 
     def test_network_colums(self):
         # test if network dataset contains necessary columns
         self.set_up()
-        df_prepare_network = accessibility.prepare_network(network_gdf=self.net, boundary=self.boundary)
+        df_prepare_network = accessibility.prepare_network(
+            network_gdf=self.net, boundary=self.boundary
+        )
         diff_columns = len(
             list(
-                set(["index", "fromnode", "tonode", "mode_walk", "mode_bike", "mode_mit", "vmax", "length",])
+                set(
+                    [
+                        "index",
+                        "fromnode",
+                        "tonode",
+                        "mode_walk",
+                        "mode_bike",
+                        "mode_mit",
+                        "vmax",
+                        "length",
+                    ]
+                )
                 - set(df_prepare_network.columns)
             )
         )
@@ -43,7 +60,10 @@ class PtACTest(unittest.TestCase):
     def test_dist_to_closest_max_dist(self):
         self.set_up()
         df_accessibility = accessibility.distance_to_closest(
-            self.pop, self.pt, network_gdf=self.net, maximum_distance=50,
+            self.pop,
+            self.pt,
+            network_gdf=self.net,
+            maximum_distance=50,
         )
         value = df_accessibility["pop"].sum()
 
@@ -71,7 +91,10 @@ class PtACTest(unittest.TestCase):
     def test_dist_to_closest_transport_system_high(self):
         self.set_up()
         df_accessibility = accessibility.distance_to_closest(
-            self.pop, self.pt_high, network_gdf=self.net, transport_system="high-capacity",
+            self.pop,
+            self.pt_high,
+            network_gdf=self.net,
+            transport_system="high-capacity",
         )
         value = df_accessibility["pop"].sum()
 
@@ -89,14 +112,19 @@ class PtACTest(unittest.TestCase):
             self.pop, self.pt_low, network_gdf=self.net, transport_system="low-capacity"
         )
         df_accessibility_high = accessibility.distance_to_closest(
-            self.pop, self.pt_high, network_gdf=self.net, transport_system="high-capacity",
+            self.pop,
+            self.pt_high,
+            network_gdf=self.net,
+            transport_system="high-capacity",
         )
         value = self.pop
         result = accessibility.calculate_sdg(
-            value, [df_accessibility_low, df_accessibility_high], population_column="pop",
+            value,
+            [df_accessibility_low, df_accessibility_high],
+            population_column="pop",
         )
         if sys.platform.startswith("win"):
-            self.assertEqual(round(result, 4), 0.957)#0.9561
+            self.assertEqual(round(result, 4), 0.957)  # 0.9561
         elif sys.platform.startswith("linux"):
             self.assertEqual(round(result, 4), 0.9614)
         elif sys.platform.startswith("macos"):
@@ -111,7 +139,9 @@ class PtACTest(unittest.TestCase):
 
     def test_project_gdf(self):
         self.set_up()
-        value = util.project_gdf(gdf=self.pop, geom_col="geometry", to_crs=None, to_latlong=False).crs
+        value = util.project_gdf(
+            gdf=self.pop, geom_col="geometry", to_crs=None, to_latlong=False
+        ).crs
         self.assertEqual(value, "epsg:32633")
 
 
