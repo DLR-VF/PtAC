95043050fd7bc96c8f5b62d59495852e8ee673b2
Yosmaoglu <Serra.Yosmaoglu@dlr.de>
2022-11-22 19:18:21 +0100

    update sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
-    "sphinx_markdown_builder",
+    # "sphinx_markdown_builder",
     "myst_parser",
     "sphinxcontrib.email",
 ]
