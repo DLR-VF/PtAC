afbb56f2989e23ea76da0ec919f1783858f9c825
niel_sm <simon.nieland@dlr.de>
2022-12-02 15:17:05 +0100

    anaconda workflow included

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ See license in LICENSE.md
 from setuptools import setup
 
 
- #def readme():
-    # with open("ptac/README.md") as f:
-        # return f.read()
+def readme():
+    with open("README.md") as f:
+        return f.read()
 
 
 setup(
@@ -18,7 +18,7 @@ setup(
     version="0.1.1a",
     author="Simon Nieland, Serra Yosmaoglu",
     author_email="Simon.Nieland@dlr.de, Serra.Yosmaoglu@dlr.de",
-    # long_description=readme(),
+    long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/DLR-VF/PtAC",
     # platforms="any",
