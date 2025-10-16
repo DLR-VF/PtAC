d0d813430a14ebe6121b383533cafa83bf4f9752
Daniel Krajzewicz <d.krajzewicz@gmail.com>
2025-10-16 15:24:14 +0200

    patching shebang and pydocs; adding metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-
-"""
-PtAC setup script.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""PtAC setup script.
 
 See license in LICENSE.md
 """
+# ===========================================================================
+__author__     = "Serra Yosmaoglu, Simon Nieland, Daniel Krajzewicz"
+__copyright__  = "Copyright 2021-2025, German Aerospace Center (DLR), Institute of Transport Research"
+__license__    = "EPL2.0"
+__version__    = "0.2.0"
+__maintainer__ = "Simon Nieland"
+__email__      = "simon.nieland@dlr.de"
+__status__     = "Production"
+# ===========================================================================
+# - https://github.com/DLR-VF/PtAC
+# - http://www.dlr.de/vf
+# ===========================================================================
 
+# --- imports ---------------------------------------------------------------
 import os
-
 from setuptools import setup
 
 
+# --- functions -------------------------------------------------------------
 def readme():
     with open("README.md") as f:
         return f.read()
@@ -24,8 +37,8 @@ else:
 setup(
     name="ptac",
     version="0.1.3a",
-    author="Simon Nieland, Serra Yosmaoglu",
-    author_email="Simon.Nieland@dlr.de, Serra.Yosmaoglu@dlr.de",
+    author="Simon Nieland, Serra Yosmaoglu, Daniel Krajzewicz",
+    author_email="Simon.Nieland@dlr.de, Serra.Yosmaoglu@dlr.de, Daniel.Krajzewicz@dlr.de",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/DLR-VF/PtAC",
